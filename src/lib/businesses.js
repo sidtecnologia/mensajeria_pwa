e5eae4ce9d4963993096bf1d7bb836713142475d
sidtecnologia <sidtecnologia@proton.me>
2026-03-10 18:03:05 -0500

    v1.0.2

--- src/lib/businesses.js
+++ src/lib/businesses.js
@@ -22,5 +22,13 @@ export const businessesConfig = [
     supabaseUrl: import.meta.env.VITE_ASADERO_SB_URL,
     supabaseKey: import.meta.env.VITE_ASADERO_SB_ANON_KEY,
     priority: 10
-  }
+  },
+{
+  id: 'negocio-4',
+  name: 'Pizza Pizza',
+  slug_url: 'https://pizzamenu.vercel.app',
+  supabaseUrl: import.meta.env.VITE_PIZZA_SB_URL,
+  supabaseKey: import.meta.env.VITE_PIZZA_SB_ANON_KEY,
+  priority: 10
+}
 ];
