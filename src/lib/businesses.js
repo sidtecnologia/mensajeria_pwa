ac14cc2203922130cd4e5d089dad2a4e2cf85c9c
sidtecnologia <sidtecnologia@proton.me>
2026-03-22 17:02:03 -0500

    v1.1.6

--- src/lib/businesses.js
+++ src/lib/businesses.js
@@ -8,7 +8,7 @@ export const businessesConfig = [
     priority: 5
   },
   {
-    id: 'negocio-3',
+    id: 'negocio-2',
     name: 'Asadero',
     slug_url: 'https://polloasado.vercel.app',
     supabaseUrl: import.meta.env.VITE_ASADERO_SB_URL,
@@ -16,7 +16,7 @@ export const businessesConfig = [
     priority: 10
   },
 {
-  id: 'negocio-4',
+  id: 'negocio-3',
   name: 'Pizza Pizza',
   slug_url: 'https://menupizza.vercel.app',
   supabaseUrl: import.meta.env.VITE_PIZZA_SB_URL,
@@ -24,11 +24,19 @@ export const businessesConfig = [
   priority: 10
 },
 {
-  id: 'negocio-5',
+  id: 'negocio-4',
   name: 'Saloon 1822',
   slug_url: 'https://saloon1822.vercel.app',
   supabaseUrl: import.meta.env.VITE_SALOON_SB_URL,
   supabaseKey: import.meta.env.VITE_SALOON_SB_ANON_KEY,
   priority: 1
+},
+{
+  id: 'negocio-5',
+  name: 'El Tablón',
+  slug_url: 'https://hamburguesaseltablon.vercel.app',
+  supabaseUrl: import.meta.env.VITE_TABLON_SB_URL,
+  supabaseKey: import.meta.env.VITE_TABLON_SB_ANON_KEY,
+  priority: 10
 }
 ];
